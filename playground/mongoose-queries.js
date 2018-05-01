320906bdde93b69bc046b0168afe12f76b7fe7b0
SandeepThakare <sandythakare9@gmail.com>
2018-05-01 18:35:32 +0530

    mongo queries done

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,36 @@
+const { ObjectID } = require('mongoose');
+const { mongoose } = require('./../server/db/mongoose');
+const { User } = require('./../server/models/users');
+
+var id = '5ae85bfe67d3c263e2443244';
+
+if(!ObjectID.isValid(id)){
+    console.log('Id is not valid'); 
+}
+
+User.find({
+    _id: id
+}).then((result) => {
+    console.log('Users -->', result)
+    console.log('-------------------------------');
+}).catch((err) => {
+    console.log(err);
+})
+
+User.findOne({
+    _id: id
+}).then((result) => {
+    console.log('Find One --> ', result)
+    console.log('-------------------------------');
+}).catch((err) => {
+    console.log(err);
+});
+
+User.findById(id).then((result) => {
+    if(!result){
+        return console.log('Id not found !!!');
+    }
+    console.log('Find By Id', result);
+}).catch((err) => {
+    console.log(err);
+});
